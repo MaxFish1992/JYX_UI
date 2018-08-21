9b193d0fe87c285baa230b82bb7dd0dc5b8f607a
MaxFish1992 <348756718@qq.com>
2018-08-21 11:15:37 +0800

    JYXUI

---
+++ ZdfFlatUI-master/ZdfFlatUI.Test/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZdfFlatUI.UserControls;
+
+namespace ZdfFlatUI.Test
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            PdfViewerWindow pdfViewer = new PdfViewerWindow
+            {
+                Owner = this,
+                Topmost = true,
+                Height = 1080,
+                Width = 1920,
+                Top = 0,
+                WindowStartupLocation = WindowStartupLocation.Manual,
+            };
+            pdfViewer.OpenPDF(@"F:\项目管理\智慧抚州\Exception Handling, VSTO.pdf");
+            pdfViewer.Show();
+        }
+    }
+}
---
+++ ZdfFlatUI-master/ZdfFlatUI/UserControls/PdfViewerWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MoonPdfLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZdfFlatUI.UserControls
+{
+    /// <summary>
+    /// PdfViewerWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PdfViewerWindow : Window
+    {
+        private MoonPdfPanel moonPdfPanel = null;
+        public PdfViewerWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 打开pdf文件
+        /// </summary>
+        /// <param name="fileName">文件完整路径</param>
+        public void OpenPDF(string fileName)
+        {
+            if (moonPdfPanel == null)
+            {
+                moonPdfPanel = new MoonPdfPanel();
+
+                moonPdfPanel.ViewType = ViewType.SinglePage;
+                moonPdfPanel.Loaded += moonPdfPanel_Loaded;
+                this.grid.Children.Add(moonPdfPanel);
+            }
+            moonPdfPanel.OpenFile(fileName);
+        }
+
+        void moonPdfPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            moonPdfPanel.ZoomToHeight();
+            moonPdfPanel.PageRowDisplay = PageRowDisplayType.ContinuousPageRows;
+            moonPdfPanel.PageMargin = new Thickness(0, 2, 0, 2);
+        }
+    }
+}
