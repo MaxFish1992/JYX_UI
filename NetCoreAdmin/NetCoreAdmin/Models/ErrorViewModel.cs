099b0b92af65a46cb2f677bb3413e2f438399ae1
MaxFish1992 <348756718@qq.com>
2018-09-19 12:03:54 +0800

    NetCoreAdmin-Start

---
+++ NetCoreAdmin/NetCoreAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
